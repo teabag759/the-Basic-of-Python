1d36e8fce72c1274863c86d11e8fe8f8877a4680
LeeSohyeon <teabag759@gmail.com>
2023-11-24 23:05:55 +0900

    First day

---
+++ Band Name Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+name_of_color = input("What's your favorite color? \n")
+
+name_of_pet_name = input("What's your pet's name? \n")
+
+print("Your band name could be " + name_of_color + " " + name_of_pet_name)
